5b472e43e417566804a7e00bb05b6d5f10dbe4fc
Your Name <sharan20051998@gmail.com>
2019-08-08 00:41:07 +0530

    Did something

--- core.py
+++ core.py
@@ -6,30 +6,29 @@ from util import progress
 from util import gather
 import config
 import numpy as np
-#need to move to static graphs: this is not working
-# input_tensor has dimensions [time_step, batch_size, vector_dimensions]
-'''
-Current Design
-3 LSTM layers: 128, 128, 64
-2 fully connected: 32(tanh), 10(softmax)
-'''
 
+# input_tensor has dimensions [time_step, batch_size, vector_dimensions]
+# Current Design
+# 3 LSTM layers: 128, 128, 64
+# 2 fully connected: 32(tanh), 10(softmax)
 # SETUP
-# input_tensors = [tf.placeholder(tf.float32) for _ in range((28 // config.window_size()) ** 2)]	# time_step x batch_size x input_dims
+
 print('Loading and preparing data', end = '')
 (images, labels), (t_images, t_labels) = tf.keras.datasets.mnist.load_data()
-images = np.asarray(images).astype(np.float32) * 2.0 / 255.0 - 1.0
-t_images = np.asarray(t_images).astype(np.float32) * 2.0 / 255.0 - 1.0
-labels = np.asarray(labels)
-t_labels = np.asarray(t_labels)
+images = np.asarray(images[: 5000]).astype(np.float32) * 2.0 / 255.0 - 1.0
+t_images = np.asarray(t_images[: 1000]).astype(np.float32) * 2.0 / 255.0 - 1.0
+labels = np.asarray(labels[: 5000])
+t_labels = np.asarray(t_labels[: 1000])
 images = gather(images, config.window_size())
 t_images = gather(t_images, config.window_size())
 TIME_STEPS = (28 // config.window_size()) ** 2
-images = np.concatenate((images, np.zeros(list(np.shape(images)[: -1]) + [2])), axis = -1)
-t_images = np.concatenate((t_images, np.zeros(list(np.shape(t_images)[: -1]) + [2])), axis = -1)
-print('Data Loaded\t\t\t')
+images = np.concatenate((images, np.zeros(list(np.shape(images)[: -1]) + [2], np.float32)), axis = -1)
+t_images = np.concatenate((t_images, np.zeros(list(np.shape(t_images)[: -1]) + [2], np.float32)), axis = -1)
+print('\rData Loaded' + '                           ')
 # print(np.shape(images), np.shape(t_images))
 # exit(0)
+
+print('Preparing Model', end = '')
 input_images = tf.placeholder(tf.float32, shape = [TIME_STEPS, None, config.window_size() ** 2 + 2])
 input_tensor_list = [input_images[i] for i in range(TIME_STEPS)]
 input_labels = tf.placeholder(tf.int32, shape = [None])
@@ -50,15 +49,13 @@ optimizer = tf.train.AdamOptimizer(learning_rate).minimize(loss)
 
 session = tf.Session()
 session.run(tf.global_variables_initializer())
+print('\rModel Initialised')
 
 
 def train(epoch, global_step):
-	# optimizer = config.optimizer(steps = global_step, epoch = epoch).minimize(loss)
-	# return {'accuracy': 10.0, 'loss': 10.0}
+	print('Epoch {:3d} {:3.2f}'.format(epoch, 0), end = '')
 	for batch in range(len(labels) // config.batch_size()):
 		L = batch * config.batch_size()
-		# print('lol')
-		progress(epoch, (L * 10.0) / len(labels))
 		R = L + config.batch_size()
 		mini_batch_images = images[: ,L: R, :]
 		mini_batch_labels = labels[L: R]
@@ -67,12 +64,14 @@ def train(epoch, global_step):
 		# grads = tape.gradient(loss, model.trainable_variables)
 		# optimizer.apply_gradients(zip(grads, model.trainable_variables), global_step=tf.train.get_or_create_global_step())
 		global_step += 1
+		print('\rEpoch {:3d} {:3.2f}'.format(epoch, L * 100.0 / len(labels)), end = '')
+	print('\rEpoch {:3d} {:3.2f}'.format(epoch, 100.0), end = '')
 
-	# loss = 0.0
-	# success = 0.0
+def test(images, labels):
 	correct_predictions = 0.0
 	total_predictions = 0.0
 	model_loss = 0.0
+	print('\rEpoch {:3d} {:3.2f}'.format(epoch, 0.0), end = '')
 	for batch in range(len(labels) // config.batch_size()):
 		L = batch * config.batch_size()
 		R = L + config.batch_size()
@@ -85,32 +84,13 @@ def train(epoch, global_step):
 		total_predictions += config.batch_size()
 		correct_predictions += output[1]
 		model_loss += output[0]
-
-	return {'accuracy': correct_predictions * 100.0 / total_predictions, 'loss': model_loss}
-
-def test():
-	correct_predictions = 0.0
-	total_predictions = 0.0
-	model_loss = 0.0
-	for batch in range(len(t_labels) // config.batch_size()):
-		L = batch * config.batch_size()
-		R = L + config.batch_size()
-		mini_batch_images = t_images[:, L: R, :]
-		mini_batch_labels = t_labels[L: R]
-		feed_dict = {input_images: mini_batch_images, input_labels: mini_batch_labels}
-		# classes = tf.argmax(logits, 1)
-		output = session.run([loss, success], feed_dict = feed_dict)
-		total_predictions += config.batch_size()
-		correct_predictions += output[1]
-		model_loss += output[0]
-
+		print('\rEpoch {:3d} {:3.2f}'.format(epoch, L * 100.0 / len(labels)), end = '')
+	print('\rEpoch {:3d} {:3.2f}'.format(epoch, 100.0), end = '')
 	return {'accuracy': correct_predictions * 100.0 / total_predictions, 'loss': model_loss}
 
 
 for epoch in range(config.epochs()):
-	results = train(epoch, global_step)
-	train_accuracy = results['accuracy']
-	progress(epoch, 10.0)
-	loss = results['loss']
-	test_accuracy = test()['accuracy']
-	publish(float(train_accuracy), float(loss), float(test_accuracy))
+	train(epoch, global_step)
+	train_data_results = test(images, labels)
+	test_data_results = test(t_images, t_labels)
+	print('\rEpoch {} {} {}'.format(epoch, train_data_results, test_data_results))
