32f82bb5a364e406c224aadb669dc80e10f77162
rabbi-shergill <53837722+rabbi-shergill@users.noreply.github.com>
2019-08-09 02:02:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,12 +10,12 @@ def epochs():
 	return 100
 
 def learning_rate(epoch, steps):	#
-	if(epoch > 5):
-		return 0.01
-	return 0.1
+# 	if(epoch > 5):
+# 		return 0.01
+	return 0.01
 
 def lstm_layers():
-	return [128, 128]
+	return [128, 128, 64]
 
 def fc_layers():
-	return []
+	return [32]
